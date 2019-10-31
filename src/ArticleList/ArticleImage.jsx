28e75209ed576c73ff40ecba818a9c2c461162d0
christine zhang <zhang.xuanw@husky.neu.edu>
2019-10-31 12:46:48 -0700

    assignment week-9

--- src/ArticleList/ArticleImage.jsx
+++ src/ArticleList/ArticleImage.jsx
@@ -4,7 +4,7 @@ import './ArticleImage.module.css';
 
 const ArticleImage = (props) => {
   return (
-    <img src= {props.article.image._url} alt={props.article.title} width = {props.article.image._width / 10} height = {props.article.image._height / 10}/>
+    <img src= {props.article.image._url} alt={props.article.title} width = {props.article.image._width / 5} height = {props.article.image._height / 5}/>
   );
 }
 ArticleImage.propTypes = {
--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import PropTypes from 'prop-types'
 import ArticleListItem from "./ArticleListItem.jsx"
-import './ArticleList.module.css'
+import styles from './ArticleList.module.css'
 
 const ArticleList = props => {
   return (
     <ul>
-      {props.articles.map(article => (
+      <h1 className = {styles.headline}>Articles</h1>
+      <div className = {styles.gridsection}>
+        {props.articles.map(article => (
         <li key={article.slug}>{<ArticleListItem article = {article}/>}</li>
-      ))}
+        ))}
+      </div>
+      
     </ul>
   );
 };
